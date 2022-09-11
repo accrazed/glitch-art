e9bae616444f89de064b2de2ceb76edc6c8c2ac1
accrazed <accrazed@gmail.com>
2022-09-11 17:40:30 -0400

    use waitgroups, add comments

--- src/channelshift/channelshift.go
+++ src/channelshift/channelshift.go
@@ -5,6 +5,7 @@ import (
 	"image"
 	"math"
 	"math/rand"
+	"sync"
 
 	"github.com/accrazed/glitch-art/src/lib"
 )
@@ -72,15 +73,16 @@ func (cs *ChannelShift) Shift() image.Image {
 	outImg := lib.CopyImage(cs.image)
 	offsetR, offsetG, offsetB, offsetA := 0, 0, 0, 0
 
-	ch := make(chan bool)
-
 	// Iterate through slices perpendicular to chunking directions
+	wg := sync.WaitGroup{}
 	for curSlice := 0; curSlice < numSlices; {
+		wg.Add(1)
+
+		// Generate volatility values, if set
 		chunkSize := cs.chunk
 		if cs.offsetVol > 0 {
 			chunkSize = cs.chunk + cs.rand.Intn(cs.chunkVol*2) - cs.chunkVol
-		}
-		if cs.offsetVol > 0 {
+
 			offsetR = (cs.rand.Int() % (cs.offsetVol * 2)) - cs.offsetVol
 			offsetG = (cs.rand.Int() % (cs.offsetVol * 2)) - cs.offsetVol
 			offsetB = (cs.rand.Int() % (cs.offsetVol * 2)) - cs.offsetVol
@@ -91,6 +93,8 @@ func (cs *ChannelShift) Shift() image.Image {
 		var cur int
 		for cur = 0; cur < chunkSize && cur+curSlice < numSlices; cur++ {
 			go func(slice, offsetR, offsetG, offsetB, offsetA int) {
+				defer wg.Done()
+
 				for pos := 0; pos < numPos; pos++ {
 					sl, ps := numSlices, numPos
 					if cs.direction == lib.Horizontal {
@@ -100,6 +104,7 @@ func (cs *ChannelShift) Shift() image.Image {
 
 					old := lib.RGBA64toPix(slice, pos, cs.image.Stride)
 
+					// Get new values
 					rX, rY := (slice+cs.translate.r.X+offsetR)%sl,
 						(pos+cs.translate.r.Y+offsetR)%ps
 					gX, gY := (slice+cs.translate.g.X+offsetG)%sl,
@@ -109,21 +114,19 @@ func (cs *ChannelShift) Shift() image.Image {
 					aX, aY := (slice+cs.translate.a.X+offsetA)%sl,
 						(pos+cs.translate.a.Y+offsetA)%ps
 
+					// Convert to 1D array position (pix)
 					newR := int(math.Abs(float64(lib.RGBA64toPix(rX, rY, cs.image.Stride))))
 					newG := int(math.Abs(float64(lib.RGBA64toPix(gX, gY, cs.image.Stride))))
 					newB := int(math.Abs(float64(lib.RGBA64toPix(bX, bY, cs.image.Stride))))
 					newA := int(math.Abs(float64(lib.RGBA64toPix(aX, aY, cs.image.Stride))))
 
-					// Red
+					// Save translation
 					outImg.Pix[old+0] = cs.image.Pix[newR+0]
 					outImg.Pix[old+1] = cs.image.Pix[newR+1]
-					// Green
 					outImg.Pix[old+2] = cs.image.Pix[newG+2]
 					outImg.Pix[old+3] = cs.image.Pix[newG+3]
-					// Blue
 					outImg.Pix[old+4] = cs.image.Pix[newB+4]
 					outImg.Pix[old+5] = cs.image.Pix[newB+5]
-					// Alpha
 					outImg.Pix[old+6] = cs.image.Pix[newA+6]
 					outImg.Pix[old+7] = cs.image.Pix[newA+7]
 
@@ -131,14 +134,11 @@ func (cs *ChannelShift) Shift() image.Image {
 						slice, pos = pos, slice
 					}
 				}
-				ch <- true
 			}(curSlice+cur, offsetR, offsetG, offsetB, offsetA)
 		}
 		curSlice += cur
 	}
-	for i := 0; i < numSlices; i++ {
-		<-ch
-	}
+	wg.Wait()
 
 	return outImg
 }
@@ -154,14 +154,14 @@ func (cs *ChannelShift) ShiftIterate() []image.Image {
 		res = append(res, cs.Shift())
 
 		if cs.animate != 1 && cs.offsetVol != 0 {
-			cs.translate.r.X = baseTr.r.X + rand.Int()%(cs.offsetVol*2) - cs.offsetVol
-			cs.translate.r.Y = baseTr.r.Y + rand.Int()%(cs.offsetVol*2) - cs.offsetVol
-			cs.translate.g.X = baseTr.g.X + rand.Int()%(cs.offsetVol*2) - cs.offsetVol
-			cs.translate.g.Y = baseTr.g.Y + rand.Int()%(cs.offsetVol*2) - cs.offsetVol
-			cs.translate.b.X = baseTr.b.X + rand.Int()%(cs.offsetVol*2) - cs.offsetVol
-			cs.translate.b.Y = baseTr.b.Y + rand.Int()%(cs.offsetVol*2) - cs.offsetVol
-			cs.translate.a.X = baseTr.a.X + rand.Int()%(cs.offsetVol*2) - cs.offsetVol
-			cs.translate.a.Y = baseTr.a.Y + rand.Int()%(cs.offsetVol*2) - cs.offsetVol
+			cs.translate.r.X = baseTr.r.X + cs.rand.Int()%(cs.offsetVol*2) - cs.offsetVol
+			cs.translate.r.Y = baseTr.r.Y + cs.rand.Int()%(cs.offsetVol*2) - cs.offsetVol
+			cs.translate.g.X = baseTr.g.X + cs.rand.Int()%(cs.offsetVol*2) - cs.offsetVol
+			cs.translate.g.Y = baseTr.g.Y + cs.rand.Int()%(cs.offsetVol*2) - cs.offsetVol
+			cs.translate.b.X = baseTr.b.X + cs.rand.Int()%(cs.offsetVol*2) - cs.offsetVol
+			cs.translate.b.Y = baseTr.b.Y + cs.rand.Int()%(cs.offsetVol*2) - cs.offsetVol
+			cs.translate.a.X = baseTr.a.X + cs.rand.Int()%(cs.offsetVol*2) - cs.offsetVol
+			cs.translate.a.Y = baseTr.a.Y + cs.rand.Int()%(cs.offsetVol*2) - cs.offsetVol
 		}
 	}
 
